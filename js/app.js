5863650ab84b27edf9a8124f2fdc64f0ef8aab5c
ayoubkandah <a.kandah994@gmail.com>
2021-01-06 17:47:12 +0200

    edited by ayoub driver

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-'use strict'
-var score=0;
+'use strict';
+var score = 0;
 //greeting with user's name
 var userName = prompt('Please enter your name ?')
 alert('Hello ' + userName + '!.. Welcome to my site')
@@ -8,60 +8,21 @@ alert('Hello ' + userName + '!.. Welcome to my site')
 var myAnswer1 = 'YES';
 var answer1 = prompt('Do you think Brown is my favourate color?');
 //check if the answer matching 
-if (answer1.toUpperCase() === 'YES' || answer1.toUpperCase() === 'Y') {
-    alert('Correct.. ' + 'Brown is not my favourate color !')
-    console.log(' Correct answer');
-    score++;
-} else {
-    alert('Wrong answer ..');
-}
+ques1();
 
 //initial var and prompt a question 
-var myAnswer2 = 'YES';
-var answer2 = prompt('Do you think Cats is my favourate pet?');
-//check if the answer matching 
-if (answer2.toUpperCase() === 'YES' || answer2.toUpperCase() === 'Y') {
-    alert('Correct.. ' + 'Cat is my favourate pet!')
-    console.log('Correct answer');
-    score++;
-} else {
-    alert('Wrong answer ..');
-}
+ques2();
 
 //initial var and prompt a question 
-var myAnswer3 = 'YES';
-var answer3 = prompt('Do you think Spring is my favourate season?');
-//check if the answer matching 
-if (answer3.toUpperCase() === 'YES' || answer3.toUpperCase() === 'Y') {
-    alert('Correct..' + 'Spring is my favourate season !')
-    console.log('Correct answer');
-    score++;
-} else {
-    alert('Wrong answer ..');
-}
+ques3();
 //initial var and prompt a question 
-var myAnswer4 = 'YES';
-var answer4 = prompt('Do you think Im a great developer?');
-//check if the answer matching 
-if (answer4.toUpperCase() === 'YES' || answer4.toUpperCase() === 'Y') {
-    alert('Correct ..' + 'Sure Im a great developer')
-    console.log('Correct answer');
-    score++;
-} else {
-    alert('Wrong answer ..');
-}
+ques4();
 
 //initial var and prompt a question 
-var myAnswer5 = 'NO';
-var answer5 = prompt('Do you think Im NOT a good cooker ?');
-//check if the answer matching 
-if (answer5.toUpperCase() === 'NO' || answer1.toUpperCase() === 'N') {
-    alert('Correct.. ' + 'I do love cooking !')
-    console.log('Correct answer');
-    score++;
-} else {
-    alert('Wrong answer ..');
-}
+ques5();
+var correctNum = 21;
+var guessNum;
+ques6();
 
 document.write('Thank you ..' + userName);
 
@@ -71,77 +32,124 @@ document.write('Thank you ..' + userName);
 // let the user guesses the correct number with just 4 chances
 
 
-var correctNum = 21;
 
-for (var numOfOportunity = 0; numOfOportunity < 4; numOfOportunity++) {
+function ques6(){
+for (var i = 0; i < 4; i++) {
+  guessNum = prompt('Guess a random number between 1 to 50 ?');
 
-    var guessNum = prompt('Guess a random number between 1 to 50 ?')
-
-    if (parseInt(guessNum) == correctNum) {
-        alert('You gessed the correct number!');
-        score++;
-    } else if (parseInt(guessNum) < correctNum) {
-        alert('too low .. try again!');
-    }
-    else if (parseInt(guessNum > correctNum)) {
-        alert('Too high .. try again!')
-    }
-
+  if (parseInt(guessNum) === correctNum) {
+    alert('You gessed the correct number!' + correctNum);
+    score++;
+    break;
+  }
+  else if (parseInt(guessNum) < correctNum) {
+    alert('too low .. try again!');
+  }
+  else if (parseInt(guessNum) > correctNum) {
+    alert('Too high .. try again!');
+  }
+  else {
+    alert('Please insert a number..');
+  }
 }
-alert('Sorry ..You take all of your possible opportunities. ' + 'The correct number is ' + correctNum);
+}
+console.log(guessNum);
 
 
- // let the user guesses answer with many possible answers with just 6 attempt
+// let the user guesses answer with many possible answers with just 6 attempt
 
-var favColor = ['purple', 'pink', 'green', 'blue', 'white', 'yellow']
+ques7();
+alert('Your total correct answers ' + score + '/7');
 
-for (var numOfAttempt = 0; numOfAttempt < 6; numOfAttempt++) {
 
-    var userColor = prompt('Guess one of my top favourate colors  ?')
-    switch (userColor.toLocaleLowerCase()) {
 
-        case favColor[0]:
-            alert('Correct guess ..');
-            score++
-            break;
 
-        case favColor[1]:
-            alert('Correct guess ..');
-            score++;
-            break;
-        case favColor[2]:
-            alert('Correct guess ..');
-            score++;
-            break;
-        case favColor[3]:
-            alert('Correct guess ..');
-            score++;
-            break;
-        case favColor[4]:
-            alert('Correct guess ..');
-            score++;
-            break;
-        case favColor[5]:
-            alert('Correct guess ..');
-            score++;
-            break;
 
-        default:
-            alert('Invalid answer');
+function ques7() {
+    var favColor = ['purple', 'pink', 'green', 'blue', 'white', 'yellow'];
+
+    var userColor = prompt('Guess one of my top favourate colors  ?');
+    var triger = true;
+    var counter = 0;
+    while (!favColor.includes(userColor) && triger) {
+        counter++;
+        if (counter === 6) {
+            triger = false;
+        }
+
+        alert('guess again');
+        userColor = prompt('Guess one of my top favourate colors  ?');
 
     }
-    alert('This is my top favourate colors'+ favColor);
+    if (favColor.includes(userColor)) {
+        score++;
+        alert('This is my top favourate colors' + favColor);
+        console.log(userColor);
 
+    }
+}
 
+function ques5() {
+    var myAnswer5 = 'NO';
+    var answer5 = prompt('Do you think Im NOT a good cooker ?');
+    //check if the answer matching 
+    if (answer5.toUpperCase() === 'NO' || answer1.toUpperCase() === 'N') {
+        alert('Correct.. ' + 'I do love cooking !');
+        console.log('Correct answer');
+        score++;
+    } else {
+        alert('Wrong answer ..');
+    }
+}
 
+function ques4() {
+    var myAnswer4 = 'YES';
+    var answer4 = prompt('Do you think Im a great developer?');
+    //check if the answer matching 
+    if (answer4.toUpperCase() === 'YES' || answer4.toUpperCase() === 'Y') {
+        alert('Correct ..' + 'Sure Im a great developer');
+        console.log('Correct answer');
+        score++;
+    } else {
+        alert('Wrong answer ..');
+    }
+}
 
+function ques3() {
+    var myAnswer3 = 'YES';
+    var answer3 = prompt('Do you think Spring is my favourate season?');
+    //check if the answer matching 
+    if (answer3.toUpperCase() === 'YES' || answer3.toUpperCase() === 'Y') {
+        alert('Correct..' + 'Spring is my favourate season !');
+        console.log('Correct answer');
+        score++;
+    } else {
+        alert('Wrong answer ..');
+    }
+}
 
+function ques2() {
+    var myAnswer2 = 'YES';
+    var answer2 = prompt('Do you think Cats is my favourate pet?');
+    //check if the answer matching 
+    if (answer2.toUpperCase() === 'YES' || answer2.toUpperCase() === 'Y') {
+        alert('Correct.. ' + 'Cat is my favourate pet!');
+        console.log('Correct answer');
+        score++;
+    } else {
+        alert('Wrong answer ..');
+    }
+}
 
-}// Total score of the 7 questions
-alert('Your total correct answers '+score+'/7');
-
-
-
+function ques1() {
+    if (answer1.toUpperCase() === 'YES' || answer1.toUpperCase() === 'Y') {
+        alert('Correct.. ' + 'Brown is not my favourate color !');
+        console.log(' Correct answer');
+        score++;
+    } else {
+        alert('Wrong answer ..');
+    }
+}
 //console.log(myNum);
 //var myNum = [1, 2, 3, 4, 5];
 
@@ -154,15 +162,3 @@ alert('Your total correct answers '+score+'/7');
 
 //for (var i = 0; i < stdNames.length; index++) {
 //  console.log(stdNames[index]);
-
-
-
-
-
-
-
-
-
-
-
-
